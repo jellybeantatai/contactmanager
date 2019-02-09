90b7ae5c3b556108bc32756891c012522e7d2834
jellybeantatai <abhinabakar@gmail.com>
2019-02-09 23:06:23 +0530

    Updates

---
+++ precache-manifest.c43361d34eab925d6752863961bab98e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "7d74642942ffa9251ff5",
+    "url": "/contactmanager/static/js/main.7d746429.chunk.js"
+  },
+  {
+    "revision": "ad5a0f0fcec8ae954d84",
+    "url": "/contactmanager/static/js/1.ad5a0f0f.chunk.js"
+  },
+  {
+    "revision": "7d74642942ffa9251ff5",
+    "url": "/contactmanager/static/css/main.e4be5b2f.chunk.css"
+  },
+  {
+    "revision": "ad5a0f0fcec8ae954d84",
+    "url": "/contactmanager/static/css/1.1131a5db.chunk.css"
+  },
+  {
+    "revision": "e0284a3d7ab1562ad2d5a1d45598061d",
+    "url": "/contactmanager/index.html"
+  }
+];
